5a2307d9f1d07317c998d5f6f659800169d6b6fb
vazkus.labs <vazkus.labs@gmail.com>
2012-03-03 12:27:46 -0800

    - getting rid of sync-related configurations...  - adding legends

--- Nidavellir/Hreidmar/hreSerialization.hpp
+++ Nidavellir/Hreidmar/hreSerialization.hpp
@@ -17,13 +17,11 @@ struct ChibiosConfig
     const static ygg::ConfigCommunication   Serialization    = ygg::COMMUNICATION_BLOCKING;
     const static ygg::ConfigCommunication   Deserialization  = ygg::COMMUNICATION_BLOCKING;
     const static ygg::ConfigEndianness      Endianness       = ygg::ENDIAN_IGNORE;
-    const static ygg::ConfigSynchronization Synchronization  = ygg::SYNC_AUTO;
     const static ygg::ConfigManifest        ManifestRequired = ygg::MANIFEST_REQUIRED;
     // various parameters of the serialization system
     const static int BasePriority = NORMALPRIO+10;
     const static int InputQueueSize = 10;
     const static int OutputQueueSize = 10;
-    const static int SyncAutoMs = 200;
     const static int ManifestRequestMs = 1000;
 };
 class ChInputHandler;
